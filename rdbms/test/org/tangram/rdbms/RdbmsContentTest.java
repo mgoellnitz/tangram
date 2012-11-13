c7acc7911f2caffa741cce8028fc7ed9e61ae902
Martin Göllnitz <martin@goellnitz.de>
2012-11-13 22:25:01 +0100

    Implement test if enhancement was done

---
+++ rdbms/test/org/tangram/rdbms/RdbmsContentTest.java
@@ -0,0 +1,24 @@
+package org.tangram.rdbms;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+import org.springframework.util.Assert;
+
+public class RdbmsContentTest {
+
+    @Test
+    public void testIsEnhanced() {
+        Method[] methods = RdbmsContent.class.getMethods();
+        boolean flag = false;
+        for (Method method : methods) {
+            System.out.println(""+method.getName());
+            if (method.getName().startsWith("jdo")) {
+                flag = true;
+            } // if
+        } // for
+        Assert.isTrue(flag, "Classes not enhanced - output unusable");
+    } // testIsEnhanced()
+
+} // RdbmsContentTest
