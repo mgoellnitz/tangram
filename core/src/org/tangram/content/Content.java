63a6d7e1e459558a41f42583b7b13cac17fc9e98
Martin Göllnitz <martin@goellnitz.de>
2013-01-15 20:22:08 +0100

    Cosmetics

--- core/src/org/tangram/content/Content.java
+++ core/src/org/tangram/content/Content.java
@@ -1,6 +1,6 @@
 /**
  * 
- * Copyright 2011 Martin Goellnitz
+ * Copyright 2011-2013 Martin Goellnitz
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,8 +21,8 @@ package org.tangram.content;
 public interface Content extends Comparable<Content> {
 
     String getId();
-    
-    
+
+
     void setBeanFactory(BeanFactory factory);
 
 
@@ -30,6 +30,6 @@ public interface Content extends Comparable<Content> {
 
 
     @Override
-	int compareTo(Content o);
+    int compareTo(Content o);
 
 } // Content
