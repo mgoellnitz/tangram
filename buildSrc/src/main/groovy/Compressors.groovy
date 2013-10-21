71e674042ecdffb736754da754a513b369e1b35c
Martin Göllnitz <martin@goellnitz.de>
2013-10-21 14:31:06 +0200

    Move tangram gradle plugin to buildSrc

---
+++ buildSrc/src/main/groovy/Compressors.groovy
@@ -0,0 +1,63 @@
+/**
+ * 
+ * Copyright 2011-2013 Martin Goellnitz
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+// Minification filter classes for JavaScript and CSS
+class JavaScriptMinify extends FilterReader {
+
+    Reader inputReader
+
+    public JavaScriptMinify(Reader inputReader) {
+        super(inputReader)
+        if (inputReader!=null) {
+            try {
+                com.yahoo.platform.yui.compressor.JavaScriptCompressor jsc = new com.yahoo.platform.yui.compressor.JavaScriptCompressor(inputReader, null)
+                StringWriter out = new StringWriter()
+                jsc.compress(out, 80, false, false, false, false)
+                out.close()
+                super.in = new StringReader(out.getBuffer().toString())
+            } catch (Exception e) {
+                ; // who cares
+            } // try/catch
+        } // if
+    } // JavaScriptMinify()
+
+} // JavaScriptMinify
+
+
+class CSSMinify extends FilterReader {
+
+    Reader inputReader
+
+    public CSSMinify(Reader inputReader) {
+        super(inputReader);
+        if (inputReader!=null) {
+            try {
+                com.yahoo.platform.yui.compressor.CssCompressor csc = new com.yahoo.platform.yui.compressor.CssCompressor(inputReader)
+                StringWriter out = new StringWriter()
+                csc.compress(out, 0)
+                out.close()
+                super.in = new StringReader(out.getBuffer().toString())
+            } catch (Exception e) {
+                ; // who cares
+            } // try/catch
+        } // if
+    } // CSSMinify()
+
+} // CSSMinify
+
+
---
+++ buildSrc/src/main/groovy/TangramVersions.groovy
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Copyright 2013 Martin Goellnitz
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+public class TangramVersions {
+
+    String servlet = '2.5'
+    String jsp = '2.0'
+    String groovy = '2.1.8'
+    String velocity = '1.7'
+    String yui = '2.4.7'
+    String junit = '4.11'
+    String jdo_api = 'javax.jdo:jdo-api:3.0.1'
+
+    String springframework = '3.1.4.RELEASE'
+    String springsecurity = '3.1.4.RELEASE'
+    // Datanucleus Version limited to 3.1.1 by Google App Engine plugin for now
+    String datanucleus = '3.1.1'
+    // The byte code enhancer is not included in every version for some reason
+    String datanucleus_enhancer = '3.1.1'
+    String datanucleus_appengine = '2.1.2'
+
+    String appengine = '1.8.6'
+  
+} // TangramVersions
