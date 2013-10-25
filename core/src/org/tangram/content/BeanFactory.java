464e86c34a79829e4b6ea2edb57244c9e02c54b7
Martin Göllnitz <martin@goellnitz.de>
2013-10-25 17:53:03 +0200

    JavaDoc

--- core/src/org/tangram/content/BeanFactory.java
+++ core/src/org/tangram/content/BeanFactory.java
@@ -1,38 +1,51 @@
 /**
- * 
- * Copyright 2011-2012 Martin Goellnitz
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Copyright 2011-2013 Martin Goellnitz
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
  */
 package org.tangram.content;
 
 import java.util.Comparator;
 import java.util.List;
 
+
 public interface BeanFactory {
 
+    /**
+     *
+     * Get a bean of a given type with a given id.
+     *
+     * The resulting bean must adhere to both conditions: id and type.
+     *
+     * @return bean with the given ID and type or null otherwise
+     */
     <T extends Content> T getBean(Class<T> cls, String id);
 
 
+    /**
+     * Get bean with a given id.
+     * @param id id of the bean to return
+     * @return bean with the given ID or null otherwise
+     */
     Content getBean(String id);
 
 
     /**
-     * 
-     * You MUST call persist() when using this method
-     * 
+     *
+     * Get a bean of a given type with a given id. You MUST call persist() after using this method! The resulting bean
+     * must adhere to both conditions: id and type.
+     *
      * @param <T>
      * @param cls
      * @param id
@@ -42,9 +55,9 @@ public interface BeanFactory {
 
 
     /**
-     * 
-     * You MUST call persist() when using this method
-     * 
+     *
+     * Get bean with a given id. You MUST call persist() after using this method! 
+     *
      * @param id
      * @return
      */
@@ -52,40 +65,58 @@ public interface BeanFactory {
 
 
     /**
-     * 
-     * @param cls
-     *            beans class to query for
-     * @param optionalQuery
-     *            query string specific to the underlying storage system
-     * @param orderProperty
-     *            name of a attribute of the bean to be used for ascending ordering
-     * @return
+     * List beans from the repository.
+     *
+     * @param cls beans class to query for
+     * @param optionalQuery query string specific to the underlying storage system
+     * @param orderProperty name of a attribute of the bean to be used for ascending ordering
+     * @return List of beans adhering the conditions - maybe empty but not null
      */
     <T extends Content> List<T> listBeans(Class<T> cls, String optionalQuery, String orderProperty, Boolean ascending);
 
 
+    /**
+     * List beans from the repository.
+     */
     <T extends Content> List<T> listBeans(Class<T> cls, String optionalQuery);
 
 
+    /**
+     * List beans from the repository.
+     */
     <T extends Content> List<T> listBeans(Class<T> cls, String optionalQuery, Comparator<T> order);
 
 
+    /**
+     * List beans from the repository.
+     */
     <T extends Content> List<T> listBeans(Class<T> cls);
 
 
+    /**
+     * List beans from the repository of an exact type. Does not take sublcasses into account.
+     */
     <T extends Content> List<T> listBeansOfExactClass(Class<T> cls, String optionalQuery, String orderProperty, Boolean ascending);
 
 
+    /**
+     * List beans from the repository of an exact type. Does not take sublcasses into account.
+     */
     <T extends Content> List<T> listBeansOfExactClass(Class<T> cls);
 
 
+    /**
+     * Create a new bean of a given type. The beans hast to be persisted in a subsequent step! The call of persist() ist
+     * mandatory after using this call.
+     */
     <T extends Content> T createBean(Class<T> cls) throws Exception;
 
 
     /**
-     * trigger this listener if changes to beans of given type occur
-     * 
-     * @param map
+     * Add a listener to be triggered if changes to beans of given type occur.
+     *
+     * @param cls type to check modified resources against
+     * @param listener listener instance to be called
      */
     void addListener(Class<? extends Content> cls, BeanListener listener);
 
