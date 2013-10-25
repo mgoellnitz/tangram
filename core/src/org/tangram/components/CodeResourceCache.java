7aed9afb9443c9a870d17894a93f80fac80586b3
Martin Göllnitz <martin@goellnitz.de>
2013-10-25 17:49:29 +0200

    Add some more support methods to code resource cache

--- core/src/org/tangram/components/CodeResourceCache.java
+++ core/src/org/tangram/components/CodeResourceCache.java
@@ -1,20 +1,18 @@
 /**
- * 
+ *
  * Copyright 2011-2013 Martin Goellnitz
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
  */
 package org.tangram.components;
 
@@ -24,6 +22,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -36,6 +35,7 @@ import org.tangram.content.BeanFactory;
 import org.tangram.content.BeanListener;
 import org.tangram.content.CodeResource;
 import org.tangram.content.TransientCode;
+
 
 @Component
 public class CodeResourceCache implements InitializingBean, BeanListener {
@@ -76,7 +76,7 @@ public class CodeResourceCache implements InitializingBean, BeanListener {
             } // if
             List<CodeResource> datastoreResources = factory.listBeans(CodeResource.class);
 
-            // Only use transient resources and cache them
+            // Only use transient resources and cache them since we can't put persistable stuff in the startup cache
             resources = new ArrayList<CodeResource>();
             for (CodeResource resource : datastoreResources) {
                 CodeResource transientResource = new TransientCode(resource);
@@ -113,28 +113,84 @@ public class CodeResourceCache implements InitializingBean, BeanListener {
     } // reset()
 
 
+    /**
+     * return a pseudo file modification time.
+     *
+     * It takes the time of last code resource scan to be used by e.g. the velocity parser again for caching purposes
+     *
+     * @return last time of scanning for code resources
+     */
     public long getLastUpdate() {
         return lastResetTime;
     } // getLastUpdate()
 
 
+    /**
+     * Obtain the code resource for a given mimetype and annotation.
+     *
+     * @param mimeType mimetype for the cache part to search in
+     * @param annotation annotation of the code resource to be returned
+     * @return a code resource if found or null otherwise
+     */
     public CodeResource get(String mimeType, String annotation) {
         Map<String, CodeResource> typeCache = resourceCache.get(mimeType);
         return (typeCache==null) ? null : typeCache.get(annotation);
     } // get()
 
 
+    /**
+     * Get a code resource for a id from the code resource cache.
+     *
+     * Quite similar to a getBean() in the bean factory
+     *
+     * @param id
+     * @return code resource for the id if found or null otherwise
+     */
     public CodeResource get(String id) {
         return cache.get(id);
     } // get()
 
 
+    /**
+     * Return a list of available mimetype in the cache.
+     *
+     * @return set of mimetypes - may be empty but not null
+     */
+    public Set<String> getTypes() {
+        return resourceCache.keySet();
+    } // getTypes()
+
+
+    /**
+     * Return all codes in the cache. unordered.
+     *
+     * @return collection of code resources - may be empty but not null
+     */
+    public Collection<CodeResource> getCodes() {
+        return cache.values();
+    } // getCodes()
+
+
+    /**
+     * Get all available annotations for a given mimetype.
+     *
+     * Since thois anotations are used like filenames this is something like a listing for the given mimetype
+     *
+     * @param mimeType
+     * @return list of annotations for the given mimetype - maybe empty but nut null
+     */
     public Collection<String> getAnnotations(String mimeType) {
         Map<String, CodeResource> typeCache = resourceCache.get(mimeType);
         return (typeCache!=null) ? typeCache.keySet() : new HashSet<String>();
     } // getAnnotations()
 
 
+    /**
+     * Return the cache portion for a given mimetype.
+     *
+     * @param mimeType
+     * @return annotation to code resource map for the given mimetype - maybe empty but not null
+     */
     public Map<String, CodeResource> getTypeCache(String mimeType) {
         Map<String, CodeResource> typeCache = resourceCache.get(mimeType);
         return (typeCache!=null) ? typeCache : new HashMap<String, CodeResource>();
@@ -145,7 +201,7 @@ public class CodeResourceCache implements InitializingBean, BeanListener {
         synchronized (attachedListeners) {
             attachedListeners.add(listener);
         } // sync
-    } // attachMapToChanges()
+    } // attachListener()
 
 
     @Override
