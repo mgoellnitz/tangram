8dbe99575632b8a316fa9f432b9e96bb02017bf7
Martin Göllnitz <martin@goellnitz.de>
2013-10-27 13:32:37 +0100

    Prepare dynamic addition of model classes

---
+++ mutable/src/org/tangram/mutable/AbstractMutableBeanFactory.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * Copyright 2013 Martin Goellnitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.tangram.mutable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.tangram.content.AbstractBeanFactory;
+import org.tangram.content.BeanListener;
+import org.tangram.content.Content;
+
+
+/**
+ * Common stuff for all bean factories dealing with mutable content.
+ */
+public abstract class AbstractMutableBeanFactory extends AbstractBeanFactory {
+
+    protected static final String QUERY_CACHE_KEY = "tangram.query.cache";
+
+    private static final Log log = LogFactory.getLog(AbstractMutableBeanFactory.class);
+
+    private Map<Class<? extends Content>, List<BeanListener>> attachedListeners = new HashMap<Class<? extends Content>, List<BeanListener>>();
+
+
+    protected Map<Class<? extends Content>, List<BeanListener>> getListeners() {
+        return attachedListeners;
+    } // getListeners() {
+
+
+    /**
+     * attach a listener for any changes dealing with classes of the given type.
+     *
+     * @param cls
+     * @param listener
+     */
+    @Override
+    public void addListener(Class<? extends Content> cls, BeanListener listener) {
+        synchronized (attachedListeners) {
+            List<BeanListener> listeners = attachedListeners.get(cls);
+            if (listeners==null) {
+                listeners = new ArrayList<BeanListener>();
+                attachedListeners.put(cls, listeners);
+            } // if
+            listeners.add(listener);
+        } // sync
+        if (log.isInfoEnabled()) {
+            log.info("addListener() "+cls.getSimpleName()+": "+attachedListeners.get(cls).size());
+        } // if
+    } // addListener()
+
+} // AbstractMutableBeanFactory
