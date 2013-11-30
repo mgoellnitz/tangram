af96e3383db32e658b8dd4eecd06cea8ceb57839
Martin Goellnitz <martin@goellnitz.de>
2013-11-30 10:25:58 +0100

    Reworked CoreMedia Adaptor part to be a tangram @LinkHandler and not a spring @Controller

--- coma/src/org/tangram/coma/ComaController.java
+++ coma/src/org/tangram/coma/ComaController.java
@@ -9,48 +9,60 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package org.tangram.coma;
 
 import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.tangram.annotate.ActionParameter;
+import org.tangram.annotate.LinkAction;
+import org.tangram.annotate.LinkHandler;
+import org.tangram.annotate.LinkPart;
 import org.tangram.controller.RenderingBase;
-import org.tangram.view.Utils;
 import org.tangram.link.Link;
+import org.tangram.link.LinkHandlerRegistry;
+import org.tangram.view.TargetDescriptor;
 
-@Controller
+
+/**
+ * LinkHandler to handle some of the default URL schemes of the CoreMedia Content Application Engine.
+ *
+ * This code still is more demo than the default link handler instance to use.
+ */
+@Named
+@LinkHandler
 public class ComaController extends RenderingBase {
 
     private static final Log log = LogFactory.getLog(ComaController.class);
 
-    @Autowired(required = false)
+    // @Autowired(required = false)
+    @Inject
     private ComaBeanPopulator populator;
 
+    @Inject
+    private LinkHandlerRegistry linkHandlerRegistry;
 
-    @RequestMapping(value = "/content/{id}")
-    public ModelAndView render(@PathVariable("id") String id,
-            @RequestParam(value = "view", required = false) String view, HttpServletRequest request,
-            HttpServletResponse response) {
+
+    @LinkAction("/content/(.*)")
+    public TargetDescriptor render(@LinkPart(1) String id, @ActionParameter(value = "view") String view, HttpServletRequest request,
+                                   HttpServletResponse response) {
         if (log.isInfoEnabled()) {
             log.info("render() id="+id);
             log.info("render() view="+view);
         } // if
-        ComaContent content = (ComaContent)beanFactory.getBean(id);
+        ComaContent content = (ComaContent) beanFactory.getBean(id);
         if (log.isInfoEnabled()) {
             log.info("render() content="+content);
         } // if
@@ -59,26 +71,25 @@ public class ComaController extends RenderingBase {
                 response.sendError(HttpServletResponse.SC_NOT_FOUND, "no content with id "+id+" in repository.");
                 return null;
             } catch (IOException ioe) {
-                return Utils.getModelAndViewFactory().createModelAndView(ioe, null, request, response);
+                return new TargetDescriptor(ioe, null, null);
             } // try/catch
         } // if
         if (populator!=null) {
             populator.populate(content);
         } // if
-        return Utils.getModelAndViewFactory().createModelAndView(content, view, request, response);
+        return new TargetDescriptor(content, view, null);
     } // render()
 
 
-    @RequestMapping(value = "/contentblob/{id}/{property}/{propertyid}")
-    public ModelAndView renderBlob(@PathVariable("id") String id, @PathVariable("property") String property,
-            @PathVariable("propertyid") String propertyId, @RequestParam(value = "view", required = false) String view,
-            HttpServletRequest request, HttpServletResponse response) {
+    @LinkAction("/contentblob/(.*)/(.*)/(.*)")
+    public TargetDescriptor renderBlob(@LinkPart(1) String id, @LinkPart(2) String property, @LinkPart(3) String propertyId,
+                                       @ActionParameter("view") String view, HttpServletRequest request, HttpServletResponse response) {
         if (log.isInfoEnabled()) {
             log.info("render() id="+id);
             log.info("render() property="+id);
             log.info("render() view="+view);
         } // if
-        ComaContent content = (ComaContent)beanFactory.getBean(id);
+        ComaContent content = (ComaContent) beanFactory.getBean(id);
         if (log.isInfoEnabled()) {
             log.info("render() content="+content);
         } // if
@@ -87,7 +98,7 @@ public class ComaController extends RenderingBase {
                 response.sendError(HttpServletResponse.SC_NOT_FOUND, "no content with id "+id+" in repository.");
                 return null;
             } catch (IOException ioe) {
-                return Utils.getModelAndViewFactory().createModelAndView(ioe, null, request, response);
+                return new TargetDescriptor(ioe, null, null);
             } // try/catch
         } // if
         if (populator!=null) {
@@ -96,7 +107,7 @@ public class ComaController extends RenderingBase {
 
         Object propertyValue = content.get(property);
 
-        return Utils.getModelAndViewFactory().createModelAndView(propertyValue, view, request, response);
+        return new TargetDescriptor(propertyValue, view, null);
     } // renderBlob()
 
 
@@ -106,13 +117,13 @@ public class ComaController extends RenderingBase {
             Link result = null;
             if (bean instanceof ComaContent) {
                 result = new Link();
-                ComaContent cc = (ComaContent)bean;
+                ComaContent cc = (ComaContent) bean;
                 String url = "/content/"+cc.getId()+(view==null ? "" : "?view="+view);
                 result.setUrl(url);
             } // if
             if (bean instanceof ComaBlob) {
                 result = new Link();
-                ComaBlob cb = (ComaBlob)bean;
+                ComaBlob cb = (ComaBlob) bean;
 
                 String url = "/contentblob/"+cb.getContentId()+"/"+cb.getPropertyName()+"/123"
                         +(view==null ? "" : "?view="+view);
@@ -122,5 +133,11 @@ public class ComaController extends RenderingBase {
         } // if
         return null;
     } // createLink()
+
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        linkHandlerRegistry.registerLinkHandler(this);
+    } // afterPropertiesSet()
 
 } // ComaController
