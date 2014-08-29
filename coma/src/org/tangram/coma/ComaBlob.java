117aa90828969f0c24e0835cf1160670369250ff
Martin Goellnitz <martin@goellnitz.de>
2014-08-30 00:55:14 +0200

    Blobsl can be handled by core

--- coma/src/org/tangram/coma/ComaBlob.java
+++ coma/src/org/tangram/coma/ComaBlob.java
@@ -1,7 +1,7 @@
 /**
- * 
+ *
  * Copyright 2011 Martin Goellnitz
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -9,26 +9,29 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package org.tangram.coma;
 
-public class ComaBlob {
-    
+import org.tangram.feature.blob.MimedBlob;
+
+
+public class ComaBlob implements MimedBlob {
+
     String contentId;
-    
+
     String propertyName;
-    
+
     String mimeType;
 
     long len;
 
-    byte[] data;
+    byte[] bytes;
 
 
     public ComaBlob(String contentId, String propertyName, String mimeType, long len, byte[] data) {
@@ -36,7 +39,7 @@ public class ComaBlob {
         this.propertyName = propertyName;
         this.mimeType = mimeType;
         this.len = len;
-        this.data = data;
+        this.bytes = data;
     } // ComaBlob()
 
 
@@ -50,6 +53,7 @@ public class ComaBlob {
     }
 
 
+    @Override
     public String getMimeType() {
         return mimeType;
     }
@@ -60,8 +64,9 @@ public class ComaBlob {
     }
 
 
-    public byte[] getData() {
-        return data;
+    @Override
+    public byte[] getBytes() {
+        return bytes;
     }
 
 } // ComaBlob
