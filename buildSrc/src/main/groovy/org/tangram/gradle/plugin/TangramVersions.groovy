e1f10517130f6957518d6bce2711e7c24bc59964
Martin Goellnitz <martin@goellnitz.de>
2015-01-03 22:56:50 +0100

    Add guice versions

--- buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
+++ buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
@@ -1,6 +1,6 @@
 /*
  * 
- * Copyright 2013-2014 Martin Goellnitz
+ * Copyright 2013-2015 Martin Goellnitz
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -47,6 +47,9 @@ public class TangramVersions {
   
   String weld = '2.2.7.Final'
   String openwebbeans = '1.2.6'
+  
+  String guice = '4.0-beta5'
+  String mycila_guice = '3.5.ga'
   
   String jdo_api = 'javax.jdo:jdo-api:3.0.1'
   String persistence_api = 'org.eclipse.persistence:javax.persistence:2.1.0'
