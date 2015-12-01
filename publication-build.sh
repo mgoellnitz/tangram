0ed801b39d259640fbc4fa86387f2aa616c9feae
Martin Goellnitz <martin@goellnitz.de>
2015-12-01 16:42:26 +0100

    Be sure to use Java 7 for CI compiling

--- publication-build.sh
+++ publication-build.sh
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-gradle -Prelease clean build jacocoCombinedReport publishToMavenLocal
+gradle -Pjdk=/opt/jdk1.7.0_80 -Prelease clean build jacocoCombinedReport publishToMavenLocal
