ce9585ae8edd695522468a52adf65a004aafa169
Martin Goellnitz <martin@goellnitz.de>
2016-02-15 13:46:39 +0100

    Update slf4j, hibernate, weld, ckeditor, and ebean dependencies

--- buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
+++ buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
@@ -33,7 +33,7 @@ public class TangramVersions {
   String groovy = '2.4.5'
   String asm = '4.0'
   String velocity = '1.7'
-  String slf4j = '1.7.14'
+  String slf4j = '1.7.16'
   String log4j = '1.2.17'
   String logback = '1.1.3'
   String yui = '2.4.7'
@@ -42,7 +42,7 @@ public class TangramVersions {
   String pac4j = '1.8.4'
   String tomcat = '7.0.67'
   String codemirror = '5.11'
-  String ckeditor = '4.5.6'
+  String ckeditor = '4.5.7'
 
   // For testing purposes
   String junit = '4.12'
@@ -56,7 +56,7 @@ public class TangramVersions {
 
   String springframework = '4.2.4.RELEASE'
 
-  String weld = '2.3.2.Final'
+  String weld = '2.3.3.Final'
   String openwebbeans = '1.6.2'
 
   // Storage options
@@ -67,7 +67,7 @@ public class TangramVersions {
 
   String openjpa = '2.4.0'
   String eclipselink = '2.6.2'
-  String hibernate = '5.0.7.Final'
+  String hibernate = '5.1.0.Final'
   String hibernate_ogm = '4.2.0.Final'
 
   // The last DataNucleus Access Plattform version to include all necessary modules
@@ -75,7 +75,7 @@ public class TangramVersions {
   // String datanucleus = '4.1.0-release'
   String datanucleus = '[4.1,4.2)'
 
-  String ebean = '6.16.2'
+  String ebean = '6.17.3'
   String ebean_agent = '4.8.1'
 
   // Not used by tangram itself
--- editor/webapp/WEB-INF/include/editing-components.jsp
+++ editor/webapp/WEB-INF/include/editing-components.jsp
@@ -2,5 +2,5 @@
 %><%
 request.setAttribute("cmprefix", "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.11.0");
 request.setAttribute("cmlibprefix", "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.11.0");
-request.setAttribute("ckprefix", "//cdn.ckeditor.com/4.5.6/standard");
+request.setAttribute("ckprefix", "//cdn.ckeditor.com/4.5.7/standard");
 %>
