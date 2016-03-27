a0b501154bc0de7f25f327669bc6d9ef8bf51940
Martin Goellnitz <martin@goellnitz.de>
2016-03-27 23:50:03 +0200

    Add basic test for google guice integration

---
+++ guicy/test/guicy/tangram-test-configurer.groovy
@@ -0,0 +1,50 @@
+
+/**
+ *
+ * Copyright 2016 Martin Goellnitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import com.google.inject.name.Names;
+import org.tangram.PersistentRestartCache
+import org.tangram.content.BeanFactory
+import org.tangram.mock.content.MockBeanFactory
+import org.tangram.util.DummyRestartCache
+import org.tangram.view.GenericPropertyConverter
+import org.tangram.view.PropertyConverter
+
+log.info "starting"
+
+log.info "configuring bean facory"
+BeanFactory beanFactory = new MockBeanFactory()
+beanFactory.init()
+module.bind(BeanFactory.class).toInstance(beanFactory)
+
+log.info "configuring property converter"
+PropertyConverter propertyConverter = new GenericPropertyConverter()
+module.bind(PropertyConverter.class).toInstance(propertyConverter)
+
+log.info "configuring persistent restart cache"
+PersistentRestartCache persistentRestartCache = new DummyRestartCache()
+module.bind(PersistentRestartCache.class).toInstance(persistentRestartCache)
+
+log.info("configuring name password mapping")
+Map<String,String> mapping = new HashMap<>()
+mapping.put('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918')
+mapping.put('user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb')
+module.bind(module.stringStringMap).annotatedWith(Names.named("usernamePasswordMapping")).toInstance(mapping)
+
+log.info "done."
+println "nucleus done."
---
+++ guicy/test/org/tangram/guicy/test/TangramServletModuleTest.java
@@ -0,0 +1,61 @@
+/*
+ *
+ * Copyright 2016 Martin Goellnitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.tangram.guicy.test;
+
+import com.google.inject.Guice;
+import com.mycila.guice.ext.closeable.CloseableModule;
+import com.mycila.guice.ext.jsr250.Jsr250Module;
+import org.tangram.Constants;
+import org.tangram.guicy.TangramServletModule;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Test aspects of the Guicy servlet module.
+ */
+public class TangramServletModuleTest {
+
+    /**
+     * Since classes in test are protected we need this wrapper.
+     */
+    public class TestServletModule extends TangramServletModule {
+
+        public void wrapConfigureServlets() {
+            configureServlets();
+            Object aggregator = getServletContext().getAttribute(Constants.ATTRIBUTE_LINK_FACTORY_AGGREGATOR);
+            Assert.assertNotNull(aggregator, "We need a link factory aggregator.");
+            Object viewSettins  = getServletContext().getAttribute(Constants.ATTRIBUTE_VIEW_SETTINGS);
+            Assert.assertNotNull(viewSettins, "We need view settings.");
+            Object statistics = getServletContext().getAttribute(Constants.ATTRIBUTE_STATISTICS);
+            Assert.assertNotNull(statistics, "We need a statistics instance.");
+            Object viewUtilities = getServletContext().getAttribute(Constants.ATTRIBUTE_VIEW_UTILITIES);
+            Assert.assertNotNull(viewUtilities, "We need a view utilities.");
+        } // wrapConfigureServlets()
+
+    } // TestServletModule
+
+
+    @Test
+    public void testTangramServletModule() {
+        TestServletModule servletModule = new TestServletModule();
+        Guice.createInjector(new CloseableModule(), new Jsr250Module(), servletModule);
+    } // testTangramServletModule()
+
+} // TangramServletModuleTest
