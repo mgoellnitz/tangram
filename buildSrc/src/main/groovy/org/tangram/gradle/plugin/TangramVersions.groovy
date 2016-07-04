c31db1818d1c403c455d1ff0a799b30bc9d26639
Martin Goellnitz <martin@goellnitz.de>
2016-07-04 10:23:01 +0200

    Update weld version string

--- buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
+++ buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
@@ -59,7 +59,7 @@ public class TangramVersions {
 
   String springframework = '4.3.0.RELEASE'
 
-  String weld = '2.3.4.Final'
+  String weld = '2.3.5.Final'
   String openwebbeans = '1.6.2'
 
   // Storage options
