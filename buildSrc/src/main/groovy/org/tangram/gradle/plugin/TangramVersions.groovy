da0619e3eb5dbb4644ad3ae0fb0d049c3e94a520
Martin Goellnitz <martin@goellnitz.de>
2016-08-09 01:11:53 +0200

    Update hibernate dependency

--- buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
+++ buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
@@ -72,7 +72,7 @@ public class TangramVersions {
 
   String openjpa = '2.4.1'
   String eclipselink = '2.6.3'
-  String hibernate = '5.2.1.Final'
+  String hibernate = '5.2.2.Final'
   String hibernate_ogm = '5.0.1.Final'
 
   // The last DataNucleus Access Plattform version to include all necessary modules
