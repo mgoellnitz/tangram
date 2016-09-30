e4080f2b525dfa75e292f7b895c679b16c29e81b
Martin Goellnitz <martin@goellnitz.de>
2016-09-30 10:43:57 +0200

    Update dependencies spring, testng, and hibernate#

--- buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
+++ buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
@@ -47,7 +47,7 @@ public class TangramVersions {
 
   // For testing purposes
   String junit = '4.12'
-  String testng = '6.9.12' // Gradle 2.13 has 6.3.1 - tests using e.g. testng listeners fail with other versions
+  String testng = '6.9.13.6' // Gradle 3.1 has 6.3.1 - tests using e.g. testng listeners fail with other versions
   String hsqldb = '1.8.1.1'
   String h2db = '1.4.192'
 
@@ -57,7 +57,7 @@ public class TangramVersions {
   String guice = '4.1.0'
   String mycila_guice = '4.0.rc1'
 
-  String springframework = '4.3.2.RELEASE'
+  String springframework = '4.3.3.RELEASE'
 
   String weld = '2.3.5.Final'
   String openwebbeans = '1.6.3'
@@ -70,7 +70,7 @@ public class TangramVersions {
 
   String openjpa = '2.4.1'
   String eclipselink = '2.6.3'
-  String hibernate = '5.1.0.Final'
+  String hibernate = '5.1.2.Final'
   String hibernate_ogm = '5.0.0.Final'
 
   // The last DataNucleus Access Plattform version to include all necessary modules
