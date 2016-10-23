696b12b522965873037be027032c268424af4894
Martin Goellnitz <martin@goellnitz.de>
2016-10-24 01:04:34 +0200

    Update Java Persistence API (JPA) interface dependency

--- buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
+++ buildSrc/src/main/groovy/org/tangram/gradle/plugin/TangramVersions.groovy
@@ -66,7 +66,7 @@ public class TangramVersions {
   String jdo_api = 'javax.jdo:jdo-api:3.1'
   // There is no official source for a JPA API jar so we decide to use one of them
   // String persistence_api = 'org.eclipse.persistence:javax.persistence:2.1.1'
-  String persistence_api = 'org.datanucleus:javax.persistence:2.1.2'
+  String persistence_api = 'org.datanucleus:javax.persistence:2.1.3'
 
   String openjpa = '2.4.1'
   String eclipselink = '2.6.3'
