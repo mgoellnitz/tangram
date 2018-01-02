0c8f9a4382072088051d3c2326d2ebb521694a4f
Martin Goellnitz <martin@goellnitz.de>
2018-01-02 02:06:15 +0100

    Try to build JDK7 compatible stuff with JDK8

--- publication-build.sh
+++ publication-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright 2015-2017 Martin Goellnitz
+# Copyright 2015-2018 Martin Goellnitz
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
